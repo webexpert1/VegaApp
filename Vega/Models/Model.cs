d044cb291cc11bed62ee9312e9fc6442ad400abb
Richard <richokoye1@gmail.com>
2021-02-06 09:57:43 +0100

    first commit

---
+++ Vega/Models/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Vega/Services/VegaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega.Models;
+
+namespace Vega.Services
+{
+    public class VegaDbContext: DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Make> Makes { get; set; }
+    }
+}
